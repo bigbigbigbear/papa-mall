0cfbbec0bd8647a50e7c2f1f54ff707c80159f09
bigbigbigbear <308355212@qq.com>
2018-04-12 09:20:48 +0800

    update

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+	//监听小程序初始化
+	onLaunch: function (options) {
+		console.log(options)
+		// 展示本地存储能力
+		var logs = wx.getStorageSync('logs') || []
+		logs.unshift(Date.now())
+		wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+		// 登录
+		wx.login({
+			success: res => {
+				// 发送 res.code 到后台换取 openId, sessionKey, unionId
+			}
+		})
+		// 获取用户信息
+		wx.getSetting({
+			success: res => {
+				if (res.authSetting['scope.userInfo']) {
+					// 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+					wx.getUserInfo({
+						success: res => {
+							// 可以将 res 发送给后台解码出 unionId
+							this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+							// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+							// 所以此处加入 callback 以防止这种情况
+							if (this.userInfoReadyCallback) {
+								this.userInfoReadyCallback(res)
+							}
+						}
+					})
+				}
+			}
+		})
+	},
+	//监听小程序显示
+	onShow: function(options){
+
+	},
+	//监听小程序隐藏
+	onHide: function () {
+
+	},
+	//当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+	onHide: function (msg) {
+		console.log(msg)
+	},
+	//当小程序出现要打开的页面不存在的情况，会带上页面信息回调该函数
+	onPageNotFound: function (res) {
+		wx.redirectTo({
+			url: 'pages/index/index'
+		})
+	},
+	globalData: {
+		userInfo: null
+	}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,92 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+	//页面的初始数据
+	data: {
+		motto: 'Hello World',
+		userInfo: {},
+		hasUserInfo: false,
+		canIUse: wx.canIUse('button.open-type.getUserInfo')
+	},
+	//事件处理函数
+	bindViewTap: function () {
+		wx.navigateTo({
+			url: '../logs/logs'
+		})
+	},
+	//监听页面加载
+	onLoad: function () {
+		if (app.globalData.userInfo) {
+			this.setData({
+				userInfo: app.globalData.userInfo,
+				hasUserInfo: true
+			})
+		} else if (this.data.canIUse) {
+			// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+			// 所以此处加入 callback 以防止这种情况
+			app.userInfoReadyCallback = res => {
+				this.setData({
+					userInfo: res.userInfo,
+					hasUserInfo: true
+				})
+			}
+		} else {
+			// 在没有 open-type=getUserInfo 版本的兼容处理
+			wx.getUserInfo({
+				success: res => {
+					app.globalData.userInfo = res.userInfo
+					this.setData({
+						userInfo: res.userInfo,
+						hasUserInfo: true
+					})
+				}
+			})
+		}
+	},
+	//监听页面初次渲染完成
+	onReady: function(){
+
+	},
+	//监听页面显示
+	onShow: function () {
+
+	},
+	//监听页面隐藏
+	onHide: function () {
+
+	},
+	//监听页面卸载
+	onUnload: function () {
+
+	},
+	//监听用户下拉
+	onPullDownRefresh: function () {
+
+	},
+	//页面上拉触底事件的处理函数
+	onReachBottom: function () {
+
+	},
+	//用户点击右上角转发
+	onShareAppMessage: function () {
+
+	},
+	//页面滚动触发事件的处理函数
+	onPageScroll: function () {
+
+	},
+	//当前是 tab 页时，点击 tab 时触发
+	onTabItemTap: function (item) {
+		console.log(item.index)
+		console.log(item.pagePath)
+		console.log(item.text)
+	},
+	getUserInfo: function (e) {
+		console.log(e)
+		app.globalData.userInfo = e.detail.userInfo
+		this.setData({
+			userInfo: e.detail.userInfo,
+			hasUserInfo: true
+		})
+	}
 })
